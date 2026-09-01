3849e892f642491d917f23c50705697bf5568f69
Philipe Fransozi <90290165+pFransozi@users.noreply.github.com>
2026-09-01 20:40:30 -0300

    Remove cards de regras da seção IA-First

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.querySelector('.semester-measurements')?.remove();
+document.querySelector('#ia-first .method-rules')?.remove();
 
 const baseScript = document.createElement('script');
 baseScript.src = 'https://cdn.jsdelivr.net/gh/pFransozi/arq-comp-so-2026-02@e8a8a5618b8cac11bc3d40439a9d608480c81b8f/script.js';
